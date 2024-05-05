eff8fbfb6400ffd6db61c324ccc97b31423ece02
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2024-05-05 17:52:44 +0200

    your website suppr

--- all_pages.py
+++ all_pages.py
@@ -1,12 +1,10 @@
 from nicegui import ui
 from pages.AboutPage import aboutPage
 from pages.ContactPage import contactPage
-from pages.YourWebsite import yourWebsite
 
 def create() -> None:
     ui.page('/Contact/')(contactPage)
     ui.page('/About/')(aboutPage)
-    ui.page('/Create-your-website')(yourWebsite)
     
 if __name__ == '__main__':
     create()
